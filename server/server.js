52905cd6ab4b9c7da29c4dd6683217681bc5ba1b
daryagolub <darya.jukova@gmail.com>
2017-08-16 20:57:15 +0200

    Add get requests suport

--- server/server.js
+++ server/server.js
@@ -26,4 +26,12 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        res.setStatus(400).send(err);
+    })
+});
+
 module.exports = {app};
